b7ee0224260e98f96990fcc6482db1d1840f0663
Furgo <furgos@qq.com>
2024-01-12 21:14:12 +0800

    starterpack abbr

---
+++ Common/Commands/GiveStarterPackAbbr.cs
@@ -0,0 +1,8 @@
+﻿namespace CTGMod.Common.Commands;
+
+public class GiveStarterPackAbbr : GiveStarterPack
+{
+    public override string Command => "sp";
+
+    public override string Description => "（简写指令）给予宝石争夺战开局礼包";
+}
